8992229d74f43602dea5534a4f3b7547228e7158
linlinfu <linlinfu@adobe.com>
2014-08-04 16:59:20 +0800

    661

--- src/config.js
+++ src/config.js
@@ -29,7 +29,7 @@ function (Settings) {
      *  +elasticsearch: {server: "http://localhost:9200", withCredentials: true}+
      *
      */
-    elasticsearch: "http://sj2slu689:9200",
+    elasticsearch: "http://sj1glm661:9200",
 
     /** @scratch /configuration/config.js/5
      *
